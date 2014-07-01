e93dba3fa95ef8215f15ab7d05cab90d58e695c5
Krzysztof Bukowski <ja@krzysztofbukowski.pl>
2014-07-01 11:10:54 +0100

    Removed file from the index

--- xhprof/includes/config.inc.php
+++
@@ -1,6 +0,0 @@
-<?php
-return array(
-	'url_base' => 'http://xhprof.local.giffgaff.com/',
-	'url_static' => null, // When undefined, it defaults to $config['url_base'] . 'public/'. This should be absolute URL.
-	'pdo' => new PDO('mysql:dbname=sch_xhprof;host=localhost;charset=utf8', 'xhprof', 'qwe123'),
-);
